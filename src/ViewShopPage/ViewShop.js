b44829b967c4025757e7184094272629c75f6140
Phiju <PhijuFile@phijus-air.lan>
2021-04-23 12:20:17 +0800

    viewShop UI

--- src/ViewShopPage/ViewShop.js
+++ src/ViewShopPage/ViewShop.js
@@ -10,23 +10,26 @@ const ViewShop = () => {
       title: "Duplicate Pants",
       category: "Bottoms",
       image:
-        "https://images.unsplash.com/photo-1558769132-92e717d613cd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8Y2xvdGhlc3xlbnwwfDJ8MHx8&auto=format&fit=crop&w=500&q=60",
-      comment: "hellofbskvskdjv jsbkjdbvjksdvbsjvk",
+        "https://d1ctous5gg780z.cloudfront.net/sites/files/younghungryfree/productimg/202103/DSC00288.jpg",
+      comment:
+        "Don't get the hype around this pants.. cutting is so off and material feels scratchy!",
       rating: 3,
       date: "21 Apr",
     },
     {
       title: "Koreen Dress",
-      category: "Top",
-      image: img,
+      category: "One-piece",
+      image:
+        "https://media.karousell.com/media/photos/products/2021/2/4/yhf_last_minute_dress_in_myrtl_1612457601_b68fccef.jpg",
       comment: "bad quality...Don't buy!!",
       rating: 1,
       date: "30 Apr",
     },
     {
-      title: "Love Bracelet",
-      category: "Accessories",
-      image: img,
+      title: "Cami Top",
+      category: "Tops",
+      image:
+        "https://d1ctous5gg780z.cloudfront.net/sites/files/younghungryfree/productimg/202102/IMG_7560.jpg",
       comment: "Awesome!!! Best top ever! super comfy and the fit is great",
       rating: 9,
       date: "3 Mar",
@@ -36,23 +39,31 @@ const ViewShop = () => {
     <div>
       <div className="search-header-container">
         <img className="search-header-img" src={lovet} alt="lovet"></img>
-        <h3 style={{ position: "absolute", color: "white" }}>Search Results</h3>
+        <h3 style={{ position: "absolute", color: "white" }}>
+          Younghungryfree
+        </h3>
       </div>
       <div style={{}}>
         {list.map((item) => (
           <div className="rating-container">
             <div
-              style={{ display: "flex", fontWeight: "bold", fontSize: "1rem" }}
+              style={{
+                display: "flex",
+                fontWeight: "bold",
+                fontSize: "1.2rem",
+                justifyContent: "space-between",
+              }}
             >
-              <p>Title: {item.title}</p>
-              <p style={{ marginLeft: "30px" }}>Category: {item.category}</p>
-              <p style={{ alignSelf: "flex-end", marginLeft: "30px" }}>
-                Rating: {item.rating}
-              </p>
+              <p>{item.title.toUpperCase()}</p>
+              <p style={{}}>{item.rating}.0</p>
             </div>
-            <p>{item.comment}</p>
+            <p style={{ marginTop: "-3px", fontSize: "1.1rem" }}>
+              {item.comment}
+            </p>
             <img className="user-img" src={item.image} alt="userImg"></img>
-            <p>Reviewed on {item.date}</p>
+            <p style={{ color: "grey", fontSize: "1rem" }}>
+              Reviewed on {item.date}
+            </p>
           </div>
         ))}
       </div>
