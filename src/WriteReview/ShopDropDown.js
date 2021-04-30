512c8bda769af749f9e8e6e85b5f192c4acfa704
Phiju <PhijuFile@phijus-air.lan>
2021-04-30 18:02:47 +0800

    added shops for dropdown

--- src/WriteReview/ShopDropDown.js
+++ src/WriteReview/ShopDropDown.js
@@ -1,35 +1,39 @@
-import React, {  useContext } from "react";
+import React, { useContext } from "react";
 import { ReviewContext } from "../context";
 
 const DropDown = () => {
-  const { setShop} = useContext(ReviewContext);
+  const { setShop } = useContext(ReviewContext);
   const onSelect = (e) => {
     setShop(e.target.value);
   };
-  
+
   return (
     <div className="shop-cat-dd-container">
       <p style={{ marginRight: "20px" }}>
-        Select
-        <span style={{ color: "#e892ad", fontWeight: "bold" }}>
-          Blogshop
-        </span>{" "}
+        Select{" "}
+        <span style={{ color: "#e892ad", fontWeight: "bold" }}>Blogshop</span>{" "}
       </p>
       <select className="shop-cat-select" onChange={(e) => onSelect(e)}>
         <option>Love Bonito</option>
         <option>Lovet</option>
-        <option>Shopsassydream</option>
-        <option>Ohvola</option>
-        <option>Supergurl</option>
-        <option>Younghungryfree</option>
-        <option>Carrislabelle</option>
-        <option>MGP</option>
-        <option>Runwaybandits</option>
         <option>Neonmello</option>
+        <option>Supergurl</option>
+        <option>Topazette</option>
+        <option>Carrislabelle</option>
+        <option>Younghungryfree</option>
+        <option>Runwaybandits</option>
         <option>Mikalya</option>
-        <option>Cheris</option>
-        <option>MDS</option>
+        <option>Ohvola</option>
+        <option>Shopsassydream</option>
         <option>Fayth</option>
+        <option>MGP</option>
+        <option>Fash Mob</option>
+        <option>Hollyhoque</option>
+        <option>Blairwears</option>
+        <option>Lbrlabel</option>
+        <option>6styleco</option>
+        <option>MGP</option>
+      
       </select>
     </div>
   );
