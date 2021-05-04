ea8de5d9a696e0cba67609940a5283dde37e3d5d
Phiju <PhijuFile@phijus-air.lan>
2021-05-04 21:58:26 +0800

    flip reveal

--- src/Home/HomeTwo.js
+++ src/Home/HomeTwo.js
@@ -4,6 +4,7 @@ import mgp from "./mgp.png";
 import fashmob from "./fashmob.png";
 import { Link } from "react-router-dom";
 import Fade from "react-reveal/Fade";
+import Flip from "react-reveal/Fade";
 
 const HomeTwo = () => {
   let popularList = [
@@ -16,25 +17,30 @@ const HomeTwo = () => {
       <Fade bottom>
         <h3 style={{ fontSize: "2rem" }}>Most Reviewed Blogshops</h3>
       </Fade>
-
-      <div className="bottom">
-        {popularList.map((shop) => (
-          <div className="bottom-div">
-            <Link
-              to={{
-                pathname: `/view-shop/${shop.shopName
-                  .replace(/\s+/g, "-")
-                  .toLowerCase()}`,
-                state: { shop: shop.shopName },
-              }}
-            >
-              <img className="s2-image" src={shop.img} alt="BlogShopImg"></img>
-            </Link>
-            <h4 style={{ fontSize: "1.3rem" }}>{shop.shopName}</h4>
-            <p>5 reviews</p>
-          </div>
-        ))}
-      </div>
+      <Flip bottom>
+        <div className="bottom">
+          {popularList.map((shop) => (
+            <div className="bottom-div">
+              <Link
+                to={{
+                  pathname: `/view-shop/${shop.shopName
+                    .replace(/\s+/g, "-")
+                    .toLowerCase()}`,
+                  state: { shop: shop.shopName },
+                }}
+              >
+                <img
+                  className="s2-image"
+                  src={shop.img}
+                  alt="BlogShopImg"
+                ></img>
+              </Link>
+              <h4 style={{ fontSize: "1.3rem" }}>{shop.shopName}</h4>
+              <p>5 reviews</p>
+            </div>
+          ))}
+        </div>
+      </Flip>
     </div>
   );
 };
